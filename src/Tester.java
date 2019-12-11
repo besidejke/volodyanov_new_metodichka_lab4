59eb0983947e48a70443b37515af7095cd004456
Volodyanov Nikita <56234640+besidejke@users.noreply.github.com>
2019-12-11 15:22:14 +0300

    Add files via upload

---
+++ src/Tester.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Tester {
+    public static void main(String[] args) {
+        Car c1 = new Car("Hyundai", "Santa Fe", "2.4 Family", 2200000, 2018);
+        c1.println();
+        Car c2 = new Car("BMW", "5 Series", "520I", 3200000, 20169);
+        c2.println();
+        Car c3 = new Car("Audi", "A8", "3.0TD", 8000000, 2017);
+        c3.println();
+    }
+}
